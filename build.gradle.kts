640e51c4fb28541fb9dc8a71eb10914f0bfebf1d
Irina <Irisha.Kalagova@yandex.ru>
2025-07-27 22:04:37 +0300

    NO problems with kapt and hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library)
     alias(libs.plugins.hilt) apply false
 
-}
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-    }
 }
